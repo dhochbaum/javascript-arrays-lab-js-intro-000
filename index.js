1f649645d83d96416352b4ed092a442d149fee2c
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 18:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten(name) {
+  
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten(name) {
+  
+}
+
+function removeFirstKitten(name) {
+  
+}
